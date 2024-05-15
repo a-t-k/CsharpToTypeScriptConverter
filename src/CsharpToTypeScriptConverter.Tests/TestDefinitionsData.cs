d16f8b96c40bdb73761408578be092b0a786021b
Alexander Tkacul <a.tkacuk@neusta.de>
2024-05-15 10:35:10 +0200

    Modify documentation

--- src/CsharpToTypeScriptConverter.Tests/TestDefinitionsData.cs
+++ src/CsharpToTypeScriptConverter.Tests/TestDefinitionsData.cs
@@ -1,15 +1,19 @@
 ﻿/// <summary>
-/// Base interface for handler search and execution.
+/// The interface is a generic type that defines a contract for request commands.
+/// The generic type parameter represents the return type of the command.
+/// The interface defines method signatures for handling the command, such as `Execute()`
+/// and `CanExecute()`
 /// </summary>
 public interface ICommand<T>;
 
 /// <summary>
-/// Interface for request command identification model binding.
+/// Simple interface for identification of request command classes
+/// It can be used for model binding in asp.net
 /// </summary>
 public interface IRequestCommand;
 
 /// <summary>
-/// Command example
+/// Simple command example.
 /// </summary>
 public class ChangeUserRoleRequestCommand: IRequestCommand, ICommand<bool>{
     public string UserId { get; set; }
@@ -17,7 +21,7 @@ public class ChangeUserRoleRequestCommand: IRequestCommand, ICommand<bool>{
 }
 
 /// <summary>
-/// Used enum in command example
+/// simple enum example
 /// </summary>
 public enum UserRoles
 {
